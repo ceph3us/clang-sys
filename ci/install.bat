314e4bcdbb34a325e71349e6332364f738bf1424
Michael Holmes <michael.holmes@protonmail.ch>
2018-06-08 16:23:14 +0100

    Try to reproduce linker searchpath issues

--- ci/install.bat
+++ ci/install.bat
@@ -3,6 +3,9 @@ rust.exe /VERYSILENT /NORESTART /DIR="C:\Rust"
 set PATH=%PATH%;C:\Rust\bin
 
 curl -sSf http://releases.llvm.org/%LLVM_VERSION%/LLVM-%LLVM_VERSION%-win32.exe -o LLVM.exe
-7z x LLVM.exe -oC:\LLVM
+7z x LLVM.exe "-oC:\LLVM (x86)\bin"
+set PATH=%PATH%;C:\LLVM (x86)\bin
+
+curl -sSf http://releases.llvm.org/%LLVM_VERSION%/LLVM-%LLVM_VERSION%-win64.exe -o LLVM.exe
+7z x LLVM.exe "-oC:\LLVM\bin"
 set PATH=%PATH%;C:\LLVM\bin
-set LIBCLANG_PATH=C:\LLVM\bin
